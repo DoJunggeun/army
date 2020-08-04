f0bb1b736bf5b9d86c79e1af3b302767074c068d
Dojunggeun <gss12049@gmail.com>
2020-08-04 14:43:20 +0000

    Updates

---
+++ precache-manifest.c1ffc2d0564bc3adbc1cc8f3fe031141.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "632bd4b4aaf5b75c0e4af7c77e807726",
+    "url": "/army/static/media/profile.632bd4b4.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/army/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c956bf419f3f3f03d85a",
+    "url": "/army/static/js/runtime~main.c956bf41.js"
+  },
+  {
+    "revision": "6daf574019ca7972fdf7",
+    "url": "/army/static/js/main.6daf5740.chunk.js"
+  },
+  {
+    "revision": "6762e6a96efb9178c489",
+    "url": "/army/static/js/1.6762e6a9.chunk.js"
+  },
+  {
+    "revision": "6daf574019ca7972fdf7",
+    "url": "/army/static/css/main.47c22f42.chunk.css"
+  },
+  {
+    "revision": "90089da1120c8cdabd12b4aaa05caad3",
+    "url": "/army/index.html"
+  }
+];
