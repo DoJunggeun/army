cafa7364737f1efae6f5ff5a89ffb6a68fa10dc0
Dojunggeun <gss12049@gmail.com>
2020-08-06 10:03:14 +0000

    getTimezoneOffset, Fragment, appname, holidaydata

--- src/App.js
+++ src/App.js
@@ -36,12 +36,12 @@ class Header extends Component {
 
 class Body extends Component {
 	render() {
-		let per = RestPercent('2020-02-24T00:00:00','2021-09-02T00:00:00');
+		const per = RestPercent('2020-02-24T00:00:00','2021-09-02T00:00:00');
 		return (
 			<div className="App-body">
 				<span className="Text-body">전역 D-{RestDays()}</span>
 				<div style={{height:13, width:'90%', backgroundColor:'white', marginBottom:'30px'}}>
-					<div style={{height:13, width:per+'%', backgroundColor:'lightgreen', alignItems:'center'}}><span style={{fontSize:13, color:'black', position:'absolute'}}>{per}%</span>
+					<div style={{height:13, width:per+'%', backgroundColor:'lightgreen', alignItems:'center'}}><span style={{fontSize:13, color:'black', position:'relative', bottom:4}}>{per}%</span>
 					</div>
 				</div>
 				<Holiday />
@@ -52,21 +52,52 @@ class Body extends Component {
 
 class Holiday extends Component {
 	render(){
-		let perholi = RestPercent('2020-07-26T20:00:00','2020-08-08T08:00:00');;
-		let peryerin = RestPercent('2020-07-26T20:00:00','2020-08-08T08:00:00');;
-		let perhg = RestPercent('2020-06-01T20:00:00','2020-09-09T08:00:00');;
-		let perend = RestPercent('2020-02-24T00:00:00','2021-09-02T00:00:00');
+		let preholi;
+		let preyerin;
+		let prehg;
+
+		for (let i = holidays.length-1 ; i >= 0  ; i-- ) {
+			if (isAfterNow(holidays[i][0]) === false) {
+				preholi = holidays[i][0];
+				break;
+			};
+		}
+		for (let i = holidays.length-1 ; i >= 0  ; i-- ) {
+			if (isAfterNow(holidays[i][0]) === false && holidays[i][2] === true) {
+				preyerin = holidays[i][0];
+				break;
+			};
+		}
+		for (let i = holidays.length-1 ; i >= 0  ; i-- ) {
+			if (isAfterNow(holidays[i][0]) === false && holidays[i][1] === '휴가') {
+				prehg = holidays[i][0];
+				break;
+			};
+		}
+		
+
+		
+		const nextholi = holidays.find(day => isAfterNow(day[0]) === true)[0];
+		const nextyerin = holidays.find(day => isAfterNow(day[0]) === true && day[2] === true)[0];
+		const nexthg = holidays.find(day => isAfterNow(day[0]) === true && day[1] === '휴가')[0];
+		
+		const perholi = RestPercent(preholi,nextholi);
+		const peryerin = RestPercent(preyerin,nextyerin);
+		const perhg = RestPercent(prehg,nexthg);
+		const perend = RestPercent('2020-02-24T00:00:00','2021-09-02T00:00:00');
+		console.log(preholi,preyerin,prehg,nextholi,nextyerin,nexthg);
+		
 		return (
 				<div className="holiday">
 					<div style={{flex:1, justifyContent:'center'}}>
 						<div style={{width:'100%'}}><b>다음 출타</b><br/>2020.08.08</div>
 						<div style={{height:13, width:'80%', backgroundColor:'white', marginBottom:12, marginLeft:'auto', marginRight:'auto'}}>
-							<div style={{height:13, width:perholi+'%', backgroundColor:'lightgreen'}}><span style={{fontSize:13, color:'black'}}>{perholi}%</span>
+							<div style={{height:13, width:perholi+'%', backgroundColor:'lightgreen'}}><span style={{fontSize:13, color:'black', position:'relative', bottom:4}}>{perholi}%</span>
 							</div>
 						</div>
 						<div style={{width:'100%'}}><b>예린이 만나는 날</b><br/>2020.08.08</div>
 						<div style={{height:13, width:'80%', backgroundColor:'white', marginLeft:'auto', marginRight:'auto'}}>
-							<div style={{height:13, width:peryerin+'%', backgroundColor:'lightgreen'}}><span style={{fontSize:13, color:'black'}}>{peryerin}%</span>
+							<div style={{height:13, width:peryerin+'%', backgroundColor:'lightgreen'}}><span style={{fontSize:13, color:'black', position:'relative', bottom:4}}>{peryerin}%</span>
 							</div>
 						</div>
 
@@ -74,7 +105,7 @@ class Holiday extends Component {
 					<div style={{flex:1}}>
 						<div style={{width:'100%'}}><b>다음 휴가</b><br/>2020.09.09</div>
 						<div style={{height:13, width:'80%', backgroundColor:'white', marginBottom:12, marginLeft:'auto', marginRight:'auto'}}>
-							<div style={{height:13, width:perhg+'%', backgroundColor:'lightgreen'}}><span style={{fontSize:13, color:'black'}}>{perhg}%</span>
+							<div style={{height:13, width:perhg+'%', backgroundColor:'lightgreen'}}><span style={{fontSize:13, color:'black', position:'relative', bottom:4}}>{perhg}%</span>
 							</div>
 						</div>	
 						<div style={{width:'100%'}}><Plan /></div>
@@ -133,10 +164,25 @@ function RestPercent(_start, _end) {
 	const today = new Date();
 	const start = new Date(_start);
 	const end = new Date(_end);
-	let per = ((32400000+today.getTime()-start.getTime())/(end.getTime()-start.getTime())*100).toFixed(2);
+	let per = ((today.getTime()-start.getTime())/(end.getTime()-start.getTime())*100).toFixed(2);
+	if (per < 0) return 0;
+	if (per > 100) return 100;
 	return per
 }
-let holidays = [
+
+function isAfterNow(date) {
+	const now = new Date();
+	const some = new Date(date);
+	if (now.getTime() < some.getTime()) return true;
+	return false;
+}
+
+const holidays = [
+	['2020-06-01T20:00:00','휴가',true],
+	['2020-06-27T20:00:00','주말외출',true],
+	['2020-07-15T21:00:00','평일외출',false],
+	['2020-07-21T21:00:00','평일외출',false],
+	['2020-07-26T20:00:00','주말외출',true],
 	['2020-08-08T08:00:00','주말외출',true],
 	['2020-08-14T17:30:00','평일외출',false],
 	['2020-08-31T17:30:00','평일외출',true],
